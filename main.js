4898eff68c1bbb6a2b93a6041c15a3440b21b584
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-09-03 17:34:06 +0530

    project finaaly working

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
-let quotes;
+const shuffleBtn = document.getElementById("shuffle");
+const adviceID = document.getElementById("adviceID");
+const adviceText = document.getElementById("adviceText");
+
+let quote;
 // Get Quotes Array from Api
 
-async function getQuotes() {
-  const APIURL = "https://type.fit/api/quotes";
+async function getNewQuote() {
+  const APIURL = "https://api.adviceslip.com/advice";
 
   try {
     const response = await fetch(APIURL);
-    quotes = await response.json();
-    console.log(quotes);
+    quote = await response.json();
+    console.log(quote);
+    adviceText.textContent = quote["slip"].advice;
+    adviceID.textContent = quote["slip"].id;
+    // Show on the screen
   } catch (error) {
     console.log(error);
   }
 }
+
+// On Load
+getNewQuote();
+
+// On Shuffle Button Click
+shuffleBtn.addEventListener("click", getNewQuote);
